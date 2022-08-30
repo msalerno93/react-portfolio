0694246ec6e98bb6c4fa0adfdb69bc78e8265094
Michael Salerno <msalerno1993@gmail.com>
2022-08-29 20:26:34 -0400

    finished skills/experience section of portfolio

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,11 +5,11 @@ const Home = () => {
   return (
     <div name='home' className='w-full h-screen bg-blue-800'>
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <h1 className='text-4xl sm:text-7xl font-bold text-black'>
+        <h1 className='text-4xl sm:text-7xl font-bold text-white'>
           Michael Salerno
         </h1>
-        <h2 className='text-4xl sm:text-7xl font-bold text-gray-400'>
-          Full Stack Developer.
+        <h2 className='text-4xl sm:text-6xl font-bold text-black'>
+          Full Stack Developer
         </h2>
         <p className='text-black font-bold py-4 max-w-[700px]'>
           I’m a full-stack developer specializing in building and designing React applications. 
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,19 +6,50 @@ import github from "../assets/github.png"
 import javascript from "../assets/javascript.png"
 import react from "../assets/react.png"
 import tailwind from "../assets/tailwind.png"
+import ruby from "../assets/ruby.png"
+import rails from "../assets/rails.png"
 
 const Skills = () => {
   return (
     <div name='skills' className='bg-blue-800'>
-        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='max-w-[1000px] mx-auto p-4 w-full h-full'>
             <div>
-                <p className='text-4xl text-blue-500 py-4 font-bold'>Experience</p>
-                <p>Technologies I've Worked With:</p>
+                <p className='flex flex-col justify-center items-center text-6xl text-white py-4 border-b-4 border-black font-bold'>Experience</p>
+                <p className='text-black font-bold text-xl py-4 '>Technologies I've Worked With:</p>
             </div>
-            <div>
-                <div>
-                    <img className='w-20 mx-auto' src={css} alt="css Icon" />
-                    <p>CSS</p>
+
+            <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+                <div className='transition duration-500 hover:scale-125'>
+                    <img className='w-20 py-4 mx-auto' src={css} alt="css Icon" />
+                    <p className='font-bold text-xl'>CSS</p>
+                </div>
+                <div className='transition duration-500 hover:scale-125'>
+                    <img className='w-20 py-4 mx-auto' src={html} alt="html Icon" />
+                    <p className='font-bold text-xl'>HTML</p>
+                </div>
+                <div className='transition duration-500 hover:scale-125'>
+                    <img className='w-20 py-4 mx-auto' src={tailwind} alt="tailwind Icon" />
+                    <p className='font-bold text-xl'>Tailwind CSS</p>
+                </div>
+                <div className='transition duration-500 hover:scale-125'>
+                    <img className='w-20 py-4 mx-auto' src={javascript} alt="javascript Icon" />
+                    <p className='font-bold text-xl'>JavaScript</p>
+                </div>
+                <div className='transition duration-500 hover:scale-125'>
+                    <img className='w-20 py-4 mx-auto' src={react} alt="react Icon" />
+                    <p className='font-bold text-xl'>React.js</p>
+                </div>
+                <div className='transition duration-500 hover:scale-125'>
+                    <img className='w-20 py-4 mx-auto' src={github} alt="github Icon" />
+                    <p className='font-bold text-xl'>GitHub</p>
+                </div>
+                <div className='transition duration-500 hover:scale-125'>
+                    <img className='w-20 py-4 mx-auto' src={ruby} alt="ruby Icon" />
+                    <p className='font-bold text-xl'>Ruby</p>
+                </div>
+                <div className='transition duration-500 hover:scale-125'>
+                    <img className='w-20 py-4 mx-auto' src={rails} alt="rails Icon" />
+                    <p className='font-bold text-xl'>Ruby on Rails</p>
                 </div>
             </div>
         </div>
